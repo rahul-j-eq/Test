1317a129fe0da03bf233e5a002961ceda6bf2db9
rahul-j-eq <rahul.jani-t@equestsolutions.net>
2025-02-12 16:18:46 +0530

    Design of initial welcome screen.

--- Explora/Controller/ViewController.swift
+++ Explora/Controller/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgOne: UIImageView!
+    @IBOutlet weak var lblExploreOne: UILabel!
+    @IBOutlet weak var lblToDesireOne: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
