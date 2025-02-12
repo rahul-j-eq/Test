abbf4f3a2edb01dd495df63cd60973017eea6bea
rahul-j-eq <rahul.jani-t@equestsolutions.net>
2025-02-12 15:00:38 +0530

    initial commit.

--- Explora/AppDelegate.swift
+++ Explora/AppDelegate.swift
@@ -14,6 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //printing out the fonts in custom library.
+        
+        UIFont.familyNames.forEach { fontNames in
+            for Names in UIFont.fontNames(forFamilyName: fontNames) {
+                print(" -- : \(Names)")
+            }
+        }
+        
         return true
     }
 
