34d53bc30e45ed2134318648e3f54db6400dac15
rahul-j-eq <rahul.jani-t@equestsolutions.net>
2025-02-12 18:15:09 +0530

    Design till login screen.

---
+++ Explora/Controller/WelcomeScreenTwoVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeScreenTwoVC.swift
+//  Explora
+//
+//  Created by Gaurav Patel on 12/02/25.
+//
+
+import UIKit
+
+class WelcomeScreenTwoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
